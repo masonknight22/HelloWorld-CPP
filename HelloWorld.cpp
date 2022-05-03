bd40ca0fefa2bfbf14f944452b05fb5f9449235c
Mason Knight <92546628+masonknight22@users.noreply.github.com>
2022-05-02 23:52:56 -0400

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,21 @@
+/**
+ * HelloWorld
+ *
+ * A program that states "Hello World!"
+ *
+ * Purdue University - CGT 272
+ *
+ * Author: Mason Knight
+ *
+ * Version: February 15 2022
+ *
+ *
+ */
+
+
+#include  <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
